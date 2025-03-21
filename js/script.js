4d5139ca72617a930c7ab8ca49aec2675fc59c6c
unknown <mohammedxp2020@gmail.com>
2025-03-21 02:51:29 +0200

    Event count down...

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// Set the date we're counting down to (Eid Ramadan)
+const countDownDate = new Date("April 1, 2025 00:00:00").getTime();
+
+// Update the countdown every 1 second
+const x = setInterval(function () {
+
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the countdown date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes, and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the elements with id="days", "hours", "minutes", "seconds"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the countdown is over, display a message
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+}, 1000);
